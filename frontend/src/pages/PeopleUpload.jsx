68ddc37e0fb8d5dabbcbb0ee88888f454a30e9b2
Andrew <67875282+Previouslynamedjeff@users.noreply.github.com>
2023-03-04 10:44:24 -0500

    aaaa

--- frontend/src/pages/PeopleUpload.jsx
+++ frontend/src/pages/PeopleUpload.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import localforage from "localforage";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUpload } from "@fortawesome/free-solid-svg-icons";
 
 function NewPersonForm() {
   return (
-    <form action="http://127.0.0.1/upload_student" method="POST" encType="multipart/form-data" className="bg-gray-200 flex flex-col gap-2 p-4" >
+    <form action="http://127.0.0.1/upload_student" method="POST" encType="multipart/form-data" className="bg-gray-200 flex flex-col gap-4 p-4" >
       <h2 className="text-2xl mb-4">Add a Student</h2>
       <input name="first_name" type="text" className="bg-white p-2 rounded-md" placeholder="Michael" />
       <input name="last_name" type="text" className="bg-white p-2 rounded-md" placeholder="Smith" />
+      <input name="student_photo" type="file" accept="image/*" />
       <button type="submit" className="bg-gray-600 text-gray-100 p-2 rounded-md hover:bg-gray-700 transition-colors">Upload</button>
     </form>
   );
