910213526950a1efad0287b2eb57408974f0af91
Andrew <67875282+Previouslynamedjeff@users.noreply.github.com>
2023-03-04 14:21:38 -0500

    menu

--- frontend/src/components/SideMenu.jsx
+++ frontend/src/components/SideMenu.jsx
@@ -4,8 +4,8 @@ function SideMenu() {
     return (
         <nav className="bg-gray-100 flex flex-col h-full w-full justify-center items-center">
             <div className="flex flex-col gap-3 text-xl">
-                <NavLink className={({ isActive }) => isActive ? "font-semibold" : "" } to="/">Students List</NavLink>
-                <NavLink className={({ isActive }) => isActive ? "font-semibold" : "" } to="/upload-attendance">Upload Attendance</NavLink>
+                <NavLink className={({ isActive }) => isActive ? "font-semibold" : "" } to="/">Student List</NavLink>
+                <NavLink className={({ isActive }) => isActive ? "font-semibold" : "" } to="/upload-attendance">Attendance Photos</NavLink>
                 <NavLink className={({ isActive }) => isActive ? "font-semibold" : "" } to="/upload-student">Upload Student</NavLink>
             </div>
         </nav>
