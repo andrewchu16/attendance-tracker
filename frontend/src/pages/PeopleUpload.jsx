2d7c6b5f9f89a1ae2bb2326961ea66b6a44245f6
Andrew <67875282+Previouslynamedjeff@users.noreply.github.com>
2023-03-04 10:49:27 -0500

    details

--- frontend/src/pages/PeopleUpload.jsx
+++ frontend/src/pages/PeopleUpload.jsx
@@ -4,10 +4,11 @@ import localforage from "localforage";
 function NewPersonForm() {
   return (
     <form action="http://127.0.0.1/upload_student" method="POST" encType="multipart/form-data" className="bg-gray-200 flex flex-col gap-4 p-4" >
-      <h2 className="text-2xl mb-4">Add a Student</h2>
-      <input name="first_name" type="text" className="bg-white p-2 rounded-md" placeholder="Michael" />
-      <input name="last_name" type="text" className="bg-white p-2 rounded-md" placeholder="Smith" />
-      <input name="student_photo" type="file" accept="image/*" />
+      <h2 className="text-2xl mb-1">Add a Student</h2>
+      <input name="first_name" type="text" className="bg-white p-2 rounded-md" placeholder="Michael" required />
+      <input name="last_name" type="text" className="bg-white p-2 rounded-md" placeholder="Smith" required />
+      <textarea name="details" className="bg-white p-2 rounded-md" placeholder="Details" required />
+      <input name="student_photo" type="file" accept="image/*" required />
       <button type="submit" className="bg-gray-600 text-gray-100 p-2 rounded-md hover:bg-gray-700 transition-colors">Upload</button>
     </form>
   );
@@ -17,7 +18,7 @@ function PeopleUpload() {
 
   return(
     <div className="mb-8 py-8 px-14">
-      <h1 className="text-4xl">Upload Students</h1>
+      <h1 className="text-4xl mb-4">Upload Students</h1>
       <NewPersonForm />
       {/* <div className="grid grid-cols-2 bg-gray-100 rounded-lg gap-8 w-full px-8 py-8">
       </div> */}
