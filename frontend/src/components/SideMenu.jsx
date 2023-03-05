e3baf077bb64f75f733c7160c13ccdbde19244c8
1spyral <83098914+1spyral@users.noreply.github.com>
2023-03-04 20:10:03 -0500

    gg!

--- frontend/src/components/SideMenu.jsx
+++ frontend/src/components/SideMenu.jsx
@@ -1,9 +1,11 @@
 import { NavLink } from "react-router-dom";
+import logo from './eye.jpg';
 
 function SideMenu() {
     return (
-        <nav className="bg-gray-100 flex flex-col h-full w-full justify-center items-center">
-            <div className="flex flex-col gap-3 text-xl">
+        <nav className="bg-gray-100 flex flex-col h-full w-full justify-start items-center">
+            <img src={logo} className="my-12"/>
+            <div className="flex flex-col gap-3 text-xl pl-2">
                 <NavLink className={({ isActive }) => isActive ? "font-semibold" : "" } to="/">Student List</NavLink>
                 <NavLink className={({ isActive }) => isActive ? "font-semibold" : "" } to="/upload-attendance">Attendance Photos</NavLink>
                 <NavLink className={({ isActive }) => isActive ? "font-semibold" : "" } to="/upload-student">Upload Student</NavLink>
